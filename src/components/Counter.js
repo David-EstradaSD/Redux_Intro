75bec296d1f6594ed193de6b935dd51c72b6fe33
David <destrada2193@gmail.com>
2021-09-29 11:03:43 -0700

    Add action payload -hardcoded for now

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -13,6 +13,10 @@ const Counter = () => {
     dispatch({ type: 'increment' });
   };
 
+  const increaseHandler = () => {
+    dispatch ({ type: 'increase', amount: 5 }); // we can add a "payload" to our action object (Store action). We hardcode a 5 but this is usually set to some user input.
+  };
+
   const decrementHandler = () => {
     dispatch({ type: 'decrement'});
   };
@@ -25,6 +29,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
